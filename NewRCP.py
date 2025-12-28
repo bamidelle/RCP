f313c633f4e0691032bb96bb6a21dfb7469dea20
bamidelle <48367095+bamidelle@users.noreply.github.com>
2025-12-28 12:17:30 +0100

    Update NewRCP.py

--- NewRCP.py
+++ NewRCP.py
@@ -1127,18 +1127,7 @@ def upgrade_user_plan(user, new_plan):
         s.commit()
 
 
-#----------------------------
-# Please log in to continue.
-#----------------------------
-def require_auth():
-    """
-    Hard stop if user is not authenticated.
-    """
-    if not st.session_state.get("authenticated"):
-        st.warning("Please log in to continue.")
-        st.stop()
 
-require_auth()
 # ----------------------
 # AUTH HELPERS
 # ----------------------
