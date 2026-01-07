f008ac1514966f666d05cf9a7c61ee2b7c8ccd05
bamidelle <48367095+bamidelle@users.noreply.github.com>
2026-01-07 06:59:14 +0100

    Update NewRCP.py

--- NewRCP.py
+++ NewRCP.py
@@ -381,7 +381,7 @@ STRIPE_PLANS = {
 # DB SETUP
 # ----------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, DB_FILE)
+#DB_PATH = os.path.join(BASE_DIR, DB_FILE)
 ENGINE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
