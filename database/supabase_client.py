5469592f6a64beebb2d91b051114b617f1ce44e5
bamidelle <48367095+bamidelle@users.noreply.github.com>
2026-02-18 08:15:44 +0100

    Refactor Supabase client to use environment secrets
    Updated Supabase credentials to use secret keys.

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from supabase import create_client
 
-SUPABASE_URL = st.secrets["https://nzpyakmlshlnvkpoizmq.supabase.coL"]
-SUPABASE_ANON_KEY = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56cHlha21sc2hsbnZrcG9pem1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTMwNzgsImV4cCI6MjA4NjU2OTA3OH0.0fU7C9YCaw2m9avPlofCJVNK-JrDULlIHu9i35hMj-8"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_ANON_KEY = st.secrets["SUPABASE_ANON_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
